4d55b5fa7df7c32a612cb85e322c73ab1e0627b3
zhuweitung <zhuweitung@foxmail.com>
2022-05-20 13:40:12 +0800

    各种类型之间转换

--- base/0_basic_type.go
+++ base/0_basic_type.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"unsafe"
 )
 
@@ -61,10 +62,14 @@ func main() {
 	fmt.Println(numberC, numberD, numberE, numberF)
 
 	defaultValue()
+	convert()
+	convertToString()
+	stringToX()
 }
 
 // 了解各种类型的默认值
 func defaultValue() {
+	fmt.Println("defaultValue...")
 	var intV int                 // 整型变量
 	var floatV float32           // 实型变量
 	var boolV bool               // 布尔型变量
@@ -99,4 +104,117 @@ func defaultValue() {
 	var structV Person                // 结构体变量
 	fmt.Println("arrary = ", arraryV) // [0, 0, 0]
 	fmt.Println("struct = ", structV) // {"" 0}
+}
+
+// 数据类型转换
+func convert() {
+	fmt.Println("convert...")
+	var num0 int = 10
+	var num1 int8 = 20
+	var num2 int16
+	//num2 = num0 // 编译报错, 不同长度的int之间也需要显示转换
+	num2 = int16(num0)
+	fmt.Println(num0, num1, num2)
+
+	var num3 float32 = 3.14
+	var num4 float64
+	//num4 = num3 // 编译报错, 不同长度的float之间也需要显示转换
+	num4 = float64(num3)
+	fmt.Println(num3, num4)
+
+	var num5 byte = 11
+	var num6 uint8
+	num6 = num5 // 这里不是隐式转换, 不报错的原因是byte的本质就是uint8
+	fmt.Println(num5, num6)
+
+	var num7 rune = 11
+	var num8 int32
+	num8 = num7 // 这里不是隐式转换, 不报错的原因是rune的本质就是int32
+	fmt.Println(num7, num8)
+
+}
+
+// 转换为字符串类型
+func convertToString() {
+	fmt.Println("convertToString...")
+	var num1 int32 = 65
+	// 可以将整型强制转换, 但是会按照ASCII码表来转换
+	var str1 string = string(num1)
+	fmt.Println(num1, str1)
+
+	//var num2 float32 = 3.14
+	//var str2 string = string(num2) // 编译报错，不能将其它基本类型强制转换为字符串类型
+	//fmt.Println(num2, str2)
+
+	//var str3 string = "97"
+	//var num3  int = int(str3) // 编译报错，不能强制转换, cannot convert str2 (type string) to type int
+	//fmt.Println(num3)
+
+	// 数字类型转字符串
+	num3 := 10
+	// 第一个参数: 需要被转换的整型,必须是int64类型
+	// 第二个参数: 转换为几进制,  必须在2到36之间
+	str4 := strconv.FormatInt(int64(num3), 10)
+	fmt.Println(num3, str4)
+
+	str5 := strconv.FormatInt(int64(num3), 2)
+	fmt.Println(str5)
+
+	var num4 float64 = 3.1234567890123456789
+	// 第一个参数: 需要转换的实型, 必须是float64类型
+	// 第二个参数: 转换为什么格式,f小数格式, e指数格式
+	// 第三个参数: 转换之后保留多少位小数, 传入-1按照指定类型有效位保留
+	// 第四个参数: 被转换数据的实际位数,float32就传32, float64就传64
+	// 将float64位实型,按照小数格式并保留默认有效位转换为字符串
+	str6 := strconv.FormatFloat(num4, 'f', -1, 32)
+	fmt.Println(str6) // 3.1234567
+	str7 := strconv.FormatFloat(num4, 'f', -1, 64)
+	fmt.Println(str7)
+	str8 := strconv.FormatFloat(num4, 'f', 2, 64)
+	fmt.Println(str8)
+
+	// 布尔类型转字符串
+	var num5 bool = true
+	str9 := strconv.FormatBool(num5)
+	fmt.Println(str9) // true
+}
+
+// 字符串类型转换为其他类型
+func stringToX() {
+	fmt.Println("stringToX...")
+
+	str1 := "127"
+	num1, err := strconv.ParseInt(str1, 10, 8)
+	fmt.Println(str1, num1, err)
+
+	str2 := "128"
+	num2, err := strconv.ParseInt(str2, 10, 8) // int8:-128~127 数据超出范围
+	fmt.Println(str2, num2, err)
+
+	str3 := "3.1234567890123456789"
+	num3, err := strconv.ParseFloat(str3, 32)
+	fmt.Println(str3, num3, err)
+
+	str4 := "false"
+	num4, err := strconv.ParseBool(str4)
+	fmt.Println(str4, num4, err)
+
+	// 字符串类型转换为数值类型时,如果不能转换除了返回error以外,还会返回对应类型的默认值
+	str5 := "abc"
+	num5, err := strconv.ParseInt(str5, 10, 32)
+	fmt.Println(str5, num5, err) // 0
+
+	// 字符串类型和整型快速转换
+	num6 := 123
+	str6 := strconv.Itoa(num6)
+	fmt.Println(num6, str6)
+
+	str7 := "129"
+	num7, err := strconv.Atoi(str7)
+	fmt.Println(str7, num7, err)
+
+	// 数据格式化
+	num8 := 110
+	str8 := fmt.Sprintf("%d", num8)
+	fmt.Println(num8, str8)
 }
