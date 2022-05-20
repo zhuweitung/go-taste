cf5b2af640baed16583429b1e30caafc64001bb9
zhuweitung <zhuweitung@foxmail.com>
2022-05-20 13:10:43 +0800

    了解各种类型的默认值

--- base/0_basic_type.go
+++ base/0_basic_type.go
@@ -60,4 +60,43 @@ func main() {
 	)
 	fmt.Println(numberC, numberD, numberE, numberF)
 
+	defaultValue()
+}
+
+// 了解各种类型的默认值
+func defaultValue() {
+	var intV int                 // 整型变量
+	var floatV float32           // 实型变量
+	var boolV bool               // 布尔型变量
+	var stringV string           // 字符串变量
+	var pointerV *int            // 指针变量
+	var funcV func(int, int) int // function变量
+	var interfaceV interface{}   // 接口变量
+	var sliceV []int             // 切片变量
+	var channelV chan int        // channel变量
+	var mapV map[string]string   // map变量
+	var errorV error             // error变量
+
+	fmt.Println("int = ", intV)                  // 0
+	fmt.Println("float = ", floatV)              // 0
+	fmt.Println("bool = ", boolV)                // false
+	fmt.Println("string = ", stringV)            // ""
+	fmt.Println("pointer = ", pointerV)          // nil
+	fmt.Println("func = ", funcV)                // nil
+	fmt.Println("interface = ", interfaceV)      // nil
+	fmt.Println("slice = ", sliceV)              // []
+	fmt.Println("slice = nil is", sliceV == nil) // true
+	fmt.Println("channel = ", channelV)          // nil
+	fmt.Println("map = ", mapV)                  // map[]
+	fmt.Println("map = nil is", mapV == nil)     // true
+	fmt.Println("error = ", errorV)              // nil
+
+	var arraryV [3]int // 数组变量
+	type Person struct {
+		name string
+		age  int
+	}
+	var structV Person                // 结构体变量
+	fmt.Println("arrary = ", arraryV) // [0, 0, 0]
+	fmt.Println("struct = ", structV) // {"" 0}
 }
